bb2096d9ea7ab5c46a81d5f7b89b5aa90a6416a2
Harsh-Agrawal96 <harshagrawal9650@gmail.com>
2025-05-21 17:04:15 +0530

    initialize socket

--- server/app.js
+++ server/app.js
@@ -2,7 +2,21 @@ import express from "express";
 import { connectDB } from "./utils/features.js";
 import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
-import { createUser } from "./seeders/user.js";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import {
+  CHAT_JOINED,
+  CHAT_LEAVED,
+  NEW_MESSAGE,
+  NEW_MESSAGE_ALERT,
+  ONLINE_USERS,
+  START_TYPING,
+  STOP_TYPING,
+} from "./constants/events.js";
+import { Message } from "./models/message.js";
+
+import {v4 as uuid} from "uuid";
+import { getSockets } from "./lib/helper.js";
 
 import userRoute from "./routes/user.js";
 import chatRoute from "./routes/chat.js";
@@ -24,8 +38,11 @@ console.log(mongoURI)
 connectDB(mongoURI);
 
 const adminSecretKey = process.env.ADMIN_SECRET_KEY || "adsasdsdfsdfsdfd";
+const userSocketIDs = new Map();
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -41,11 +58,61 @@ app.get("/", (req,res) => {
 })
 
 
+io.on("connection", (socket) => {
+
+    console.log("a user conneted", socket.id);
+    const user = {
+        _id : "isodflks",
+        name : "what is happening"
+    }
+    userSocketIDs.set(user._id.toString(), socket.id);
+
+    socket.on(NEW_MESSAGE, async ({ chatId, members, message }) => {
+        const messageForRealTime = {
+        content: message,
+        _id: uuid(),
+        sender: {
+            _id: user._id,
+            name: user.name,
+        },
+        chat: chatId,
+        createdAt: new Date().toISOString(),
+        };
+
+        const messageForDB = {
+        content: message,
+        sender: user._id,
+        chat: chatId,
+        };
+
+        const membersSocket = getSockets(members);
+        io.to(membersSocket).emit(
+            NEW_MESSAGE,
+            {
+              chatId,
+              message: messageForRealTime,
+            }
+        );
+        io.to(membersSocket).emit(NEW_MESSAGE_ALERT, { chatId });
+
+        try {
+        await Message.create(messageForDB);
+        } catch (error) {
+        throw new Error(error);
+        }
+  });
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
+
+
 app.use(errorMiddleware);
 
-app.listen( Port, () => {
+server.listen( Port, () => {
     console.log(`Server is runing at ${Port} port in ${envMode}`);
 })
 
 
-export { envMode, adminSecretKey };
+export { envMode, adminSecretKey, userSocketIDs };
